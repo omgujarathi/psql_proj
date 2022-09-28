44812a94191ae93a1bdbbeb07b6cf59eb392b44d
mayur-kidi <mayur.kidi@noovosoft.com>
2022-09-28 18:35:24 +0530

    all frontend files

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 var express = require('express')
 var router = require('./routes')
-const { Pool, Client } = require('pg')
-
+// const { Pool, Client } = require('pg')
 
 app = express()
 app.use('', router)
 
-const client = new Client({
-    user: 'mayurkidi',
-    host: 'localhost',
-    database: 'sql_playground',
-    password: '01001000',
-})
+// const client = new Client({
+//     user: 'mayurkidi',
+//     host: 'localhost',
+//     database: 'sql_playground',
+//     password: '01001000',
+// })
 
-client.connect().then(() => {
-    console.log("DB Connected")
-}).catch((err) => {
-    console.log(err)
-})
+// client.connect().then(() => {
+//     console.log("DB Connected")
+// }).catch((err) => {
+//     console.log(err)
+// })
 
 app.listen(3000, () => {
     console.log("server started")
 })
+
+// module.exports = client
--- routes.js
+++ routes.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const router = express.Router()
+const bodyParser = require('body-parser')
+const { Pool, Client } = require('pg')
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'sql_playground',
+    password: '01001000',
+})
+client.connect().then(() => {
+    console.log("DB Connected.")
+}).catch((err) => {
+    console.log(err)
+})
 
 router.get('', (req, res) => {
     res.sendFile(__dirname + '/login.html')
@@ -14,6 +30,24 @@ router.get('/registration', (req, res) => {
     res.sendFile(__dirname + '/registration.html')
 })
 
+router.post('/registration', urlencodedParser, (req, res) => {
+
+    const query = {
+        text: 'INSERT INTO users(firstname, lastname, username, user_password, score, user_role) VALUES($1, $2, $3, $4, $5, $6)',
+        values: [req.body.firstname, req.body.lastname, req.body.username, req.body.password, '0', 'player'],
+    }
+
+    client.query(query, (err, result) => {
+        if (err) {
+            console.log(err.stack)
+            alert("Error In Registration.")
+        } else {
+            alert("Registration Suceesful.")
+            res.sendFile(__dirname + '/login.html')
+        }
+    })
+
+})
 
 router.get('/user_dashboard', (req, res) => {
     res.sendFile(__dirname + '/user_dashboard.html')
