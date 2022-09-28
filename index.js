61b046d3a50c9a95fa9b688a406382bd8a324200
mayur-kidi <mayur.kidi@noovosoft.com>
2022-09-28 16:06:57 +0530

    desinging

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 var express = require('express')
 var router = require('./routes')
+const { Pool, Client } = require('pg')
+
 
 app = express()
-app.use('',router)
-app.listen(3000,()=>{
+app.use('', router)
+
+const client = new Client({
+    user: 'mayurkidi',
+    host: 'localhost',
+    database: 'sql_playground',
+    password: '01001000',
+})
+
+client.connect().then(() => {
+    console.log("DB Connected")
+}).catch((err) => {
+    console.log(err)
+})
+
+app.listen(3000, () => {
     console.log("server started")
 })
--- routes.js
+++ routes.js
@@ -2,8 +2,33 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 
-router.get('/login',(req,res)=>{
-    res.sendFile(__dirname+'/login.html')
+router.get('', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+})
+
+router.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/login.html')
+})
+
+router.get('/registration', (req, res) => {
+    res.sendFile(__dirname + '/registration.html')
+})
+
+
+router.get('/user_dashboard', (req, res) => {
+    res.sendFile(__dirname + '/user_dashboard.html')
+})
+
+router.get('/questions', (req, res) => {
+    res.sendFile(__dirname + '/questions.html')
+})
+
+router.get('/addquestions', (req, res) => {
+    res.sendFile(__dirname + '/add_questions.html')
+})
+
+router.get('/answer', (req, res) => {
+    res.sendFile(__dirname + '/answer.html')
 })
 
 module.exports = router
